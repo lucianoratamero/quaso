3acea9a1cd579dce58e689410d511a3063953551
Luciano Ratamero <luciano@ratamero.com>
2026-02-02 13:16:03 -0300

    itsTime

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  $effect(() => {
+    setTimeout(() => {
+      window.location.href = "https://twitch.tv/platinummeg";
+    }, 5000);
+  });
+</script>
+
+<div class="flex p-4 justify-center items-center h-lvh">
+  <a class="w-full max-w-4xl" href="https://twitch.tv/platinummeg">
+    <img class="h-full w-full rounded-2xl" alt="It's time" src="itstime.webp" />
+  </a>
+</div>
