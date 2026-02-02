bb8ad3e67703cf5ed758f12f5c3bc78c69911aa6
Luciano Ratamero <luciano@ratamero.com>
2026-02-02 13:25:40 -0300

    adapter vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-static';
+import { mdsvex } from "mdsvex";
+import adapter from "@sveltejs/adapter-vercel";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: { adapter: adapter() },
-	preprocess: [mdsvex()],
-	extensions: ['.svelte', '.svx']
+  kit: { adapter: adapter() },
+  preprocess: [mdsvex()],
+  extensions: [".svelte", ".svx"],
 };
 
 export default config;
